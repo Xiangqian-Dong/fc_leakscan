229eb6c56e5099ff88171237b93abbaf40ced818
Xiangqian Dong <dongxq@s-ec.com>
2020-06-08 15:02:42 +0800

    updated don.sh

--- don.sh
+++ don.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-dir=/home/xiang/project/web-waf
+dir=/home/xiang/project/firecloud/
 cd ${dir}
-tar cfz nginx-src.tar.gz nginx/
-ansible 192.168.20.99 -m copy -a "src=./nginx-src.tar.gz dest=/root/waf"
-ansible 192.168.20.99 -m shell -a "/root/waf/dongx.sh"
+tar cfz fc_leakscan-src.tar.gz fc_leakscan/
+ansible 192.168.20.91 -m copy -a "src=./fc_leakscan-src.tar.gz dest=/root/dong"
+ansible 192.168.20.91 -m shell -a "/root/dong/doleakscan.sh"
 
